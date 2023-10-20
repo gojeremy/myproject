6844a29271ab5ce4848b4140b3cf18dec4e033be
dolphin <gojeremyyoutub@gmail.com>
2023-10-20 07:19:00 +0300

    updated factory&seed

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Offer;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use App\Models\Post;
 
@@ -15,11 +16,27 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
-     //   Post::factory(50)->create();
+        Post::factory(50)->create([
+                'category' => 'general',
+            ]
+        );
+        Post::factory(50)->create([
+                'category' => 'entertainment',
+            ]
+        );
+        Post::factory(50)->create([
+                'category' => 'business',
+            ]
+        );
+        Post::factory(50)->create([
+                'category' => 'general',
+            ]
+        );
         Offer::factory(100)->create();
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         User::factory()->create([
+             'name' => 'Test User',
+             'email' => 'test@test',
+             'password' => '12345678',
+         ]);
     }
 }
